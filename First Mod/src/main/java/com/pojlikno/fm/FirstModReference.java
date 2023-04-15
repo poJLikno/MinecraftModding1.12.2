82a5b029d8e7b85d8f89f80851f1c5231e92db43
poJLikno <pojlikno@gmail.com>
2023-04-15 21:30:52 +0300

    Small update

--- First Mod/src/main/java/com/pojlikno/fm/FirstModReference.java
+++ First Mod/src/main/java/com/pojlikno/fm/FirstModReference.java
@@ -1,7 +1,7 @@
 package com.pojlikno.fm;
 
 public class FirstModReference {
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String MOD_NAME = "First Mod";
 	public static final String MOD_ID = "fm";
 	public static final String ACCEPTED_VERSIONS = "[1.12]";
